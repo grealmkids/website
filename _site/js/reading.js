d2b4144279f2dadc7d8680d7489cc0135008896f
alfredochola <ochalfie@gmail.com>
2023-06-10 15:51:59 +0300

    added the G-Realm pack

---
+++ _site/js/reading.js
@@ -0,0 +1,7 @@
+function reading1() {
+    document.getElementById("reading1").style.display = "block";
+  }
+
+  function close1() {
+    document.getElementById("reading1").style.display = "none";
+  }
